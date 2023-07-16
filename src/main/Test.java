aaeb72e19452b5d79010cf66836e1996592ada8e
Strangedip <sg02295@gmail.com>
2023-07-16 18:59:53 +0530

    as

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,11 +1,6 @@
 package main;
 
-import java.io.Console;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 import util.Validate;
-
-import util.Msg;
 
 public class Test {
     public static void main(String[] args) {
--- src/user/CreateAccount.java
+++ src/user/CreateAccount.java
@@ -1,7 +1,6 @@
 package user;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+
 
 import util.Action;
 import util.Msg;
