b1b202fcfa2bd71e11062586ad96a13e3c21de1f
Strangedip <sg02295@gmail.com>
2023-07-17 14:37:53 +0530

    psv conf

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,33 +1,14 @@
 package main;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
 
 import util.Msg;
 
 public class Test {
     static Scanner in=new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println(mobileNumber());
+        String a=in.next();
+        a=a.concat("a");
+        System.out.println(a);
     }
 
-    private static long mobileNumber(){
-    try {
-            Msg.border();
-            System.out.print("Enter Mobile Number: ");
-            long num = in.nextLong();
-            int digits=(int)Math.log10(num);
-            System.out.println(digits+1);
-            if (digits+1==10) {
-                return num;
-
-            } else {
-                System.out.println("Invalid Input, try Again");
-                return mobileNumber();
-            }
-        } catch (Exception e) {
-            System.out.println("Invalid Input, try Again");
-    }
-    return mobileNumber();
-}
 }
--- src/util/TakeInput.java
+++ src/util/TakeInput.java
@@ -1,14 +1,5 @@
 package util;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class TakeInput {
-    public static void psw() {
 
-    }
-
-    public static void email() {
-
-    }
 }
