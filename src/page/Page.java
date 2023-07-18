cbf4244263cdc031d0508ce4886aa5e64185a793
Strangedip <sg02295@gmail.com>
2023-07-18 09:42:38 +0530

    updated

--- src/page/Page.java
+++ src/page/Page.java
@@ -6,4 +6,17 @@ public class Page {
     public static void homePage(User user) {
         user.details();
     }
+
+    public static void shop(){
+        
+    }
+    public static void accountInfo(){
+
+    }
+    public static void hunt(){
+
+    }
+    public static void updateAccountDetails(){
+
+    }
 }
