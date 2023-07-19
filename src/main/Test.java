163708a266b099464f5edc9c8dfaba719dc8331c
Strangedip <sg02295@gmail.com>
2023-07-19 10:09:49 +0530

    test

--- src/main/Test.java
+++ src/main/Test.java
@@ -6,15 +6,7 @@ public class Test {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a = in.nextInt();
-        switch (a) {
-            case 1:
-                System.out.println("case1");
-                break;
-            case 2:
-                System.out.println("case2");
-        }
-
+        
     }
 
 }
