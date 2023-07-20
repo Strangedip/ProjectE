5ba1b4a6c7736deb7be58e09b9b8214cf116ac27
Strangedip <sg02295@gmail.com>
2023-07-20 23:32:55 +0530

    exception handling in page option input and comments added

--- src/page/HomePage.java
+++ src/page/HomePage.java
@@ -1,12 +1,13 @@
 package page;
 
-import java.util.Scanner;
 
 import user.User;
 import util.Msg;
 
 //Home Page class containing format and action of home page 
 public class HomePage {
+
+    // home page Page details method
     public static void page(User user) {
         Msg.header();
         user.details();
--- src/page/Page.java
+++ src/page/Page.java
@@ -8,11 +8,13 @@ import util.Msg;
 
 // page class containing all page calling methods
 public class Page {
+    // profile page shows details of the user
     public static void profilePage(User user) {
         user.details();
         ;
     }
 
+    // calls homepage page method
     public static void homePage(User user) {
         HomePage.page(user);
     }
@@ -33,32 +35,40 @@ public class Page {
 
     }
 
+    // options available at every page
     public static void homePageOptions() {
         System.out.println("1.View Profile    2.Shopping         3.Reward Hunt");
         System.out.println("4.Update Profile  5.Account Details  6.Log out");
-        System.out.print("Action Input -> ");
     }
 
+    // input for homePageOptions
     public static void pageInput(User user) {
+        System.out.print("Action Input -> ");
         Scanner in = new Scanner(System.in);
-        switch (in.nextInt()) {
-            case 1:
-                Page.profilePage(user);
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                Msg.loggedOut();
-                Home.menu(user);
-                break;
-            default:
-                System.out.println("Invalid Option");
+        try {
+            switch (in.nextInt()) {
+                case 1:
+                    Page.profilePage(user);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    Msg.loggedOut();
+                    Home.menu(user);
+                    break;
+                default:
+                    System.out.println("Invalid Option");
+                    pageInput(user);
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid Input");
+            pageInput(user);
         }
     }
 }
