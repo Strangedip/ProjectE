924e48bfb2165ba9a7b733c90489119e3ca08145
Strangedip <sg02295@gmail.com>
2023-07-31 18:54:01 +0530

    testing cyclic sort

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,19 +1,28 @@
 package main;
 
+import java.util.Arrays;
 
 public class Test {
 
     public static void main(String[] args) {
-        solution(25, 2500);
-        
+        int[] a= {0,1,8,6,5,4,3,2,7};
+        sort(a);
+        System.out.println(Arrays.toString(a));   
     }
 
-    public static void solution(int x, int y) {
-        if(x>y*10){
-            System.out.println("spam");
-        }
-        else{
-            System.out.println("genuine");
+    public static void sort(int[] a) {
+        int i=0;
+        while(i<a.length){
+            System.out.println(i);
+            System.out.println(a[i]+" "+a[a[i]]);
+            if(a[i]!=i){
+                int temp=a[a[i]];
+                a[a[i]]=a[i];
+                a[i]=temp;
+            }
+            else{
+                i++;
+            }
         }
     }
 
