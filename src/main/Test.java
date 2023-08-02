d2054efea0913aaef8b7cf155b1bd7ebd16d346c
Strangedip <sg02295@gmail.com>
2023-08-02 15:23:21 +0530

    cyclic sort test

--- src/main/Test.java
+++ src/main/Test.java
@@ -5,25 +5,22 @@ import java.util.Arrays;
 public class Test {
 
     public static void main(String[] args) {
-        int[] a= {0,1,8,6,5,4,3,2,7};
+        int[] a = { 3, 1, 4, 6, 5, 8, 0, 2, 7 };
+        System.out.println(Arrays.toString(a));
         sort(a);
-        System.out.println(Arrays.toString(a));   
+        System.out.println(Arrays.toString(a));
     }
 
     public static void sort(int[] a) {
-        int i=0;
-        while(i<a.length){
-            System.out.println(i);
-            System.out.println(a[i]+" "+a[a[i]]);
-            if(a[i]!=i){
-                int temp=a[a[i]];
-                a[a[i]]=a[i];
-                a[i]=temp;
-            }
-            else{
+        int i = 0;
+        while (i < a.length) {
+            if (a[i] != i) {
+                int temp = a[a[i]];
+                a[a[i]] = a[i];
+                a[i] = temp;
+            } else {
                 i++;
             }
         }
     }
-
 }
