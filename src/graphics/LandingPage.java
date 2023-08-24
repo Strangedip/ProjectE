49019ae147bd0d1cf29a7149a50b8ed7a30dc927
Strangedip <sg02295@gmail.com>
2023-08-25 03:26:40 +0530

    UI

---
+++ src/graphics/LandingPage.java
@@ -0,0 +1,12 @@
+package graphics;
+
+import javax.swing.JFrame;
+
+public class LandingPage {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ProjectE Landing Page");
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
